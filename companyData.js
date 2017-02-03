2f33cf6b01e9151144e14185b0262e92eaa3e7ce
Joel <japoncea@gmail.com>
2017-02-03 14:22:00 -0800

    adding companyData.js

---
+++ companyData.js
@@ -0,0 +1,121 @@
+var companyData = [{
+  company: "Yahoo!"
+  year: 2016
+  overall: {
+  	white: 45,
+	asian: 44,
+	latinx: 4,
+	black: 2,
+	other: 5
+  }
+  leadership: {
+	white: 72,
+	asian: 22,
+	latinx: 3,
+	black: 0,
+	other: 4
+  }
+  tech: {
+	white: 31,
+	asian: 62,
+	latinx: 2,
+	black: 1,
+	other: 4
+  }
+  nonTech: {
+	white: 67,
+	asian: 18,
+	latinx: 6,
+	black: 3,
+	other: 7
+}, {
+  company: "Google"
+  year: 2016 
+  overall: {
+	white: 59,
+	asian: 32,
+	latinx: 3,
+	black: 2,
+	other: 4
+  }
+  leadership: {
+	white: 70,
+	asian: 25,
+	latinx: 1,
+	black: 2,
+	other: 3
+  }
+  tech: {
+	white: 57,
+	asian: 37,
+	latinx: 3,
+	black: 1,
+	other: 4
+  }
+  nonTech: {
+	white: 63,
+	asian: 23,
+	latinx: 5,
+	black: 4,
+	other: 5
+}, {
+  company: "Facebook"
+  year: 2016
+  overall: {
+	white: 52,
+	asian: 38,
+	latinx: 4,
+	black: 2,
+	other: 4
+  }
+  leadership: {
+	white: 71,
+	asian: 21,
+	latinx: 3,
+	black: 3,
+	other: 3
+  }
+  tech: {
+	white: 48,
+	asian: 46,
+	latinx: 3,
+	black: 1,
+	other: 3
+  }
+  nonTech: {
+	white: 60,
+	asian: 25,
+	latinx: 7,
+	black: 5,
+	other: 4
+},{
+  company: "Apple"
+  year: 2016
+  overall: {
+	white: 56,
+	asian: 19,
+	latinx: 12,
+	black: 9,
+	other: 3
+  }
+  leadership: {
+	white: 67,
+	asian: 21,
+	latinx: 7,
+	black: 3,
+	other: 1
+  }
+  tech: {
+	white: 55,
+	asian: 27,
+	latinx: 8,
+	black: 8,
+	other: 3
+  }
+  nonTech: {
+	white: 58,
+	asian: 12,
+	latinx: 16,
+	black: 11,
+	other: 4
+}]
